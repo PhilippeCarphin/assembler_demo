4b578b9a795c57042432f77ceb4e3a7ab3d26c27
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-11-05 03:15:26 -0500

    No nonsense add function call
    with no usage of %rbp whatsoever

--- main.phil.s
+++ main.phil.s
@@ -9,22 +9,17 @@ fmt_int:
 	.string "An int : %d\n"
 
 main:
-	# Save stack frame
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $8, %rsp # Allocate stack space for int a at -8(%rbp)
+	subq $8, %rsp
 
-	# int a = add(77,88) function call
+	# a = add(77, 88) : ...
 	push $88 # 88)
 	push $77 # 77,
-	# call add # add(
-	jmp add
-	pushq %rip
-after_add:
+	call add # add(
 	addq $16, %rsp # cleanup
 	movq %rax, -8(%rbp) # a =
 
-	# Compare a to expected return value
 	cmpl $165, -8(%rbp)
 	jne test_failed
 test_passed:
@@ -35,18 +30,10 @@ test_failed:
 	movq $failed_str, %rdi
 	call puts
 done:
-	# Deallocate memeory
-	addq $8, %rsp
-	# Set return value
 	movq $0, %rax
-	# Restore stack frame
-	popq %rbp
-	# Jump to address after the call
 	ret
 
 add:
-	# Calculate return value
 	movq (%rsp), %rax
 	addq 8(%rsp), %rax
-	# Jump back to caller
-	jmp after_add
+	ret
